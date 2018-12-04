2f3c1b25a68e4b735d2371a92e8246ef9bd118f8
JoJoNatto <43765840+JoJoNatto@users.noreply.github.com>
2018-12-03 21:05:27 -0800

    changed some file name

---
+++ heatmap_data/amazon_data.R
@@ -0,0 +1,73 @@
+library(shiny)
+library(R.utils)
+library(data.table)
+library(ggplot2)
+library(maps)
+library(mapproj)
+library(lubridate)
+library(dplyr)
+library(gtrendsR)
+
+amazon_state <- data.table::fread("googlestate/state-pop-amazon.csv")
+amazon_state <- amazon_state[-c(4, 26), ]
+amazon_state$Region <- tolower(amazon_state$Region)
+amazon_state <- rename(amazon_state, region = Region)
+amazon_state_copy <- amazon_state
+
+amazon_trend <- data.table::fread("googlesearch/AMAZ_trend.csv") 
+amazon_trend <- amazon_trend[-c(261), ]
+
+state_abb <- data.table::fread("state-abbreviations.csv") 
+state_abb <- state_abb[-c(2, 12), ]
+state_abb$State <- tolower(state_abb$State)
+state_abb <- rename(state_abb, region = State)
+amazon_state <- merge(state_abb,amazon_state, by="region", all.x=T) #%>% 
+amazon_state_name <- amazon_state
+
+google.trends = gtrends(c("amazon"), geo = "US-WA", gprop = "web", time = "2013-12-01 2018-11-18")
+google.trends <- mutate(google.trends$interest_over_time, date = as.Date(date, format = "%m/%d/%Y"))
+
+for (i in 1:49) {
+  data <- gtrends("amazon", geo = paste("US", amazon_state_name[i, "Abbreviation"], sep = "-"), 
+                  gprop = "web", time = "2013-12-01 2018-11-18")$interest_over_time
+  data <- mutate(data, date = paste(as.Date(date, format = "%m-%d-%Y"))) %>% 
+    select("date", "hits") 
+  names(data)[2] <- paste(amazon_state_name[i, "region"])
+  write.csv(data, file = paste("amazon_state_trends/", amazon_state_name[i, "region"], ".csv", sep = ""), row.names = FALSE)
+  print(i)
+}
+
+states_final <- amazon_trend %>% rename(date = Week)
+for (i in 1:49) {
+  states_final <- merge(states_final, 
+                        data.table::fread(paste("amazon_state_trends/", amazon_state_name[i, "region"], ".csv", sep = "")), 
+                        by="date", all.x=T)
+  print(i)
+}
+
+states_final <- states_final[, -c(1:2)] * amazon_trend$`Amazon.com: (United States)`
+states_final$date <- amazon_trend$Week
+states_final <- as.data.frame(t(states_final))
+for (i in 1:260) {
+  colnames(states_final)[i] <- as.character(amazon_trend[, "Week"][i])
+  print(i)
+}
+setDT(states_final, keep.rownames = TRUE)[]
+states_final <- rename(states_final, region = rn)
+states_final <- states_final[-c(50), ]
+
+states <- map_data("state")
+map.df <- merge(states,states_final, by="region", all.x=T)
+map.df <- map.df[order(map.df$order),]
+for (i in 7:266) {
+  map.df[, i]=as.numeric(levels(map.df[, i]))[map.df[, i]]
+}
+a <- ggplot(map.df, aes(x=long,y=lat,group= map.df$group))+
+  geom_polygon(aes(fill=`2014-04-06`))+
+  geom_path()+ 
+  scale_fill_gradientn(colours=rev(heat.colors(10)),na.value="grey90", limits=c(0,10000))+
+  coord_map()
+print(a)
+write.csv(map.df,'processed data/amazon.csv', row.names = FALSE)
+
+
---
+++ heatmap_data/facebook_data.R
@@ -0,0 +1,67 @@
+library(shiny)
+library(R.utils)
+library(data.table)
+library(ggplot2)
+library(maps)
+library(mapproj)
+library(lubridate)
+library(dplyr)
+library(gtrendsR)
+
+fb_state <- data.table::fread("googlestate/state-pop-facebook.csv")
+fb_state <- fb_state[-c(which(grepl("Hawaii", fb_state$Region)), which(grepl("Alaska", fb_state$Region))), ]
+fb_state$Region <- tolower(fb_state$Region)
+fb_state <- rename(fb_state, region = Region)
+
+fb_trend <- data.table::fread("googlesearch/FB_trend.csv") 
+fb_trend <- fb_trend[-c(261), ]
+
+state_abb <- data.table::fread("state-abbreviations.csv") 
+state_abb <- state_abb[-c(2, 12), ]
+state_abb$State <- tolower(state_abb$State)
+state_abb <- rename(state_abb, region = State)
+fb_state <- merge(state_abb,fb_state, by="region", all.x=T) #%>% 
+fb_state_name <- fb_state
+
+for (i in 1:49) {
+  data <- gtrends("facebook", geo = paste("US", fb_state_name[i, "Abbreviation"], sep = "-"), 
+                  gprop = "web", time = "2013-12-01 2018-11-18")$interest_over_time
+  data <- mutate(data, date = paste(as.Date(date, format = "%m-%d-%Y"))) %>% 
+    select("date", "hits") 
+  names(data)[2] <- paste(fb_state_name[i, "region"])
+  write.csv(data, file = paste("fb_state_trends/", fb_state_name[i, "region"], ".csv", sep = ""), row.names = FALSE)
+  print(i)
+}
+
+states_final <- fb_trend %>% rename(date = Week)
+for (i in 1:49) {
+  states_final <- merge(states_final, 
+                        data.table::fread(paste("facebook_state_trends/", fb_state_name[i, "region"], ".csv", sep = "")), 
+                        by="date", all.x=T)
+  print(i)
+}
+
+states_final <- states_final[, -c(1:2)] * fb_trend$`Facebook, Inc.: (United States)`
+states_final$date <- fb_trend$Week
+states_final <- as.data.frame(t(states_final))
+for (i in 1:260) {
+  colnames(states_final)[i] <- as.character(fb_trend[, "Week"][i])
+  print(i)
+}
+setDT(states_final, keep.rownames = TRUE)[]
+states_final <- rename(states_final, region = rn)
+states_final <- states_final[-c(50), ]
+
+states <- map_data("state")
+map.df <- merge(states,states_final, by="region", all.x=T)
+map.df <- map.df[order(map.df$order),]
+for (i in 7:266) {
+  map.df[, i]=as.numeric(levels(map.df[, i]))[map.df[, i]]
+}
+a <- ggplot(map.df, aes(x=long,y=lat,group= map.df$group))+
+  geom_polygon(aes(fill=`2018-07-22`))+
+  geom_path()+ 
+  scale_fill_gradientn(colours=rev(heat.colors(10)),na.value="grey90", limits=c(0,10000))+
+  coord_map()
+print(a)
+write.csv(map.df,'processed data/facebook.csv', row.names = FALSE)
